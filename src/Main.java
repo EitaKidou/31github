eacad3074ba2e12aa0e9460827db128bd97ad7ab
Абдурашид <149584797+EitaKidou@users.noreply.github.com>
2023-12-04 19:32:28 +0600

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -81,3 +81,21 @@ class Solution1 {
 //        }
 //    }}
 
+//class Solution3 {
+//    public:
+//    int removeDuplicates(vector<int> &nums) {
+//        if (nums.empty()) {
+//            return 0;
+//        }
+//
+//        int k = 1; // Initialize the count of unique elements to 1
+//        for (int i = 1; i < nums.size(); i++) {
+//            if (nums[i] != nums[i - 1]) {
+//                nums[k] = nums[i]; // Overwrite the next unique element
+//                k++;
+//            }
+//        }
+//
+//        return k;
+//    }
+//};
